1ff4dcef9d75653367f9d5fb33b519886259decd
deyvidandrades <deyvid-asilva@hotmail.com>
2025-04-18 09:00:55 -0300

    * Gradle upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.deyvidandrades.meuclima"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.deyvidandrades.meuclima"
-        minSdk = 31
-        targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.4"
+        minSdk = 33
+        targetSdk = 35
+        versionCode = 6
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -35,21 +35,21 @@ android {
 dependencies {
 
     //Core libs
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 
     //Android widgets
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-beta01")
 
     //Outras bibliotecas
     implementation("com.google.code.gson:gson:2.11.0")
-    implementation("com.google.android.play:review:2.0.1")
-    implementation("com.google.android.play:review-ktx:2.0.1")
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("com.google.android.play:review:2.0.2")
+    implementation("com.google.android.play:review-ktx:2.0.2")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 
     //Test libraries

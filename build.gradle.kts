bec7e10fcf200587c0ba1a23b3c467d3b3c4a909
deyvidandrades <deyvid-asilva@hotmail.com>
2026-07-23 18:05:18 -0300

    * Gradle upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all subprojects/modules. 8.13.2
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
 }
